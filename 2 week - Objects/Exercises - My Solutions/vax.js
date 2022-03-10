7781027db41d40097c850dbed1c284e0a6a6c67f
PavlaManova <48806251+PavlaManova@users.noreply.github.com>
2022-03-10 19:05:03 +0200

    add solution for fence in triangular shape

--- 2 week - Objects/Exercises - My Solutions/vax.js
+++ 2 week - Objects/Exercises - My Solutions/vax.js
@@ -17,24 +17,24 @@ function vaxInit()
 	document.body.appendChild( renderer.domElement );
 	document.body.style.margin = 0;
 	document.body.style.overflow = 'hidden';
-	
-	stats = new Stats();
-	document.body.appendChild( stats.dom );
-	
+
+	/*stats = new Stats();
+	document.body.appendChild( stats.dom );*/
+
 	scene = new THREE.Scene();
 	scene.background = new THREE.Color('white');
 
-	camera = new THREE.PerspectiveCamera( 60, 1, 1, 1000 );
-	camera.position.set(0,0,100);
-	camera.lookAt(new THREE.Vector3(0,0,0));
-	
+  camera = new THREE.PerspectiveCamera(60,window .innerWidth/window.innerHeight,1,100);
+  camera.position.set(0, 0, 50);
+  camera.lookAt(new THREE.Vector3(0,0,0));
+
 	light = new THREE.PointLight();
 	light.position.set(0,150,300);
 	scene.add( light );
-	
+
 	window.addEventListener( 'resize', onWindowResize, false );
 	onWindowResize();
-	
+
 	renderer.setAnimationLoop( frame );
 }
 
@@ -45,15 +45,14 @@ function onWindowResize( event )
 	camera.updateProjectionMatrix();
 
 	renderer.setSize( window.innerWidth, window.innerHeight, true );
-}			
+}
 
 
 function frame( time )
 {
 	if (animate) animate( time/1000 );
-	
-	stats.update();
-	
+
+	//stats.update();
+
 	renderer.render( scene, camera );
 }
-
